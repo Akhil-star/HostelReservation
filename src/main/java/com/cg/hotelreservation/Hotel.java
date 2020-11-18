325cfbc9bd8401b2ca5d5ca59c7a1610734dcf12
Akhil-star <akhilakki1305@gmail.com>
2020-11-18 17:20:48 +0530

    Add weekdays rate and weekend rate

--- src/main/java/com/cg/hotelreservation/Hotel.java
+++ src/main/java/com/cg/hotelreservation/Hotel.java
@@ -5,6 +5,8 @@ public class Hotel {
     public double rate;
     public String startDate;
     public String endDate;
+    public double weekdayRate;
+    public double weekendRate;
     public Hotel(String hotelName, double rate) {
         this.hotelName = hotelName;
         this.rate = rate;
@@ -14,6 +16,16 @@ public class Hotel {
         this.hotelName = hotelName;
         this.rate = rate;
         this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Hotel(String hotelName, double weekdayRate, double weekendRate, String startDate, String endDate) {
+        this.hotelName = hotelName;
+        this.weekdayRate = weekdayRate;
+        this.weekendRate = weekendRate;
+        this.startDate = startDate;
+        this.startDate = startDate;
+        this.endDate = endDate;
         this.endDate = endDate;
     }
 }
--- src/test/java/com/cg/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/cg/hotelreservation/HotelReservationTest.java
@@ -34,4 +34,15 @@ public class HotelReservationTest {
         Hotel cheapestHotel = hotelReservation.findCheapestHotel("2020-01-01", "2020-12-31");
         Assert.assertEquals("Lakewood", cheapestHotel.hotelName);
     }
+
+    @Test
+    public void givenWeekendWeekdayRates_WhenHotelCreated_ShouldNotBeNull()
+    {
+        Hotel hotel1 = new Hotel("Lakewood", 110, 90, "2020-01-25", "2020-01-26");
+        Hotel hotel2 = new Hotel("Bridgewood", 160, 50, "2020-02-12", "2020-02-14");
+        Hotel hotel3 = new Hotel("Ridgewood", 220, 150, "2020-11-01", "2020-11-04");
+        Assert.assertNotNull(hotel1);
+        Assert.assertNotNull(hotel2);
+        Assert.assertNotNull(hotel3);
+    }
 }
